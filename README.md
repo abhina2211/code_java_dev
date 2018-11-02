033d5bc616f828f2735e57499747a88dd345325b
Abhinav Bhargava <abhinavb@m1.com.sg>
2018-11-02 23:39:35 +0800

    Committing code with solution for problem A.2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # code_java_dev
 Repository for the code assignment for Java developer
+
+
+#This is the solution description file for Problem A.2
+
+Since the code should be able to handle different type of birds I have restructured the code to make use of Factory Patterns.
+
+By using this pattern we can not only make the code maintainable but also scalable.I  have also used the accessor and mutator to retrieve and update animalName.
+BirdFactory class can later be modified to include the behavior of parrot.
+
--- Solution.java
+++ Solution.java
@@ -1,27 +1,76 @@
+class Animal{
+    private String animalName;
+    Animal(String animalName){
+        this.animalName = animalName;
+    }
+    protected String getAnimalName() {
+        return animalName;
+    }
+    protected void setAnimalName(String animalName){
+        this.animalName = animalName;
+    }
 
-class Animal {
-	void walk()
-	{
-	System.out.println("I am walking");
-	}
+    String walk(){
+        return String.format("%s can walk.", animalName);
+    }
 }
 
-class Bird extends Animal{
-	
-	void fly(){
-		System.out.println("I am flying");
-	}
-	void sing(){
-		System.out.println("I am singing");
-	}
+class Bird extends Animal {
+    private String song;
+    private Boolean canSwim;
+    private Boolean canFly;
 
+    Bird(String birdName){
+        super(birdName);
+    }
+    Bird(String birdName, String song) {
+        this(birdName);
+        this.song = song;
+    }
+    Bird(String birdName, String song, Boolean canSwim,Boolean canFly){
+        this(birdName,song);
+        this.canSwim = canSwim == null ? false : canSwim;
+        this.canFly = canFly == null ? false : canFly;
+    }
+    String sing() {
+        return String.format("%s says: %s", getAnimalName(),song);
+    }
+    String swim() {
+        return canSwim ? String.format("%s can swim.", getAnimalName()) : String.format("%s can't swim.", getAnimalName());
+    }
+    String fly() {
+        return canFly ? String.format("%s can fly.", getAnimalName()) : String.format("%s can't fly.", getAnimalName());
+    }
+}
+
+class BirdFactory {
+
+    public static Bird getBird(String birdName){
+        if(birdName == null){
+            return null;
+        }
+        if ("Duck".equalsIgnoreCase(birdName)){
+            return new Bird(birdName, "Quack Quack", true,true);
+        } else if ("Chicken".equalsIgnoreCase(birdName)){
+            return new Bird(birdName, "Cluck Cluck", false,false);
+        }
+        return null;
+    }
 }
 
 public class Solution {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Bird bird = new Bird();
-		bird.walk();
-		bird.sing();
-}
+
+    public static void main(String[] args){
+        Bird duck = BirdFactory.getBird("Duck");
+        System.out.println(duck.walk());
+        System.out.println(duck.fly());
+        System.out.println(duck.sing());
+        System.out.println(duck.swim());
+        System.out.println("--------------------------");
+        Bird chicken = BirdFactory.getBird("Chicken");
+        System.out.println(chicken.walk());
+        System.out.println(chicken.fly());
+        System.out.println(chicken.sing());
+        System.out.println(chicken.swim());
+    }
 }
